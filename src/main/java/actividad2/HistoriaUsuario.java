a9bad4a2d33051491a323a47479227f19f0cdab5
BenjaminRo01 <benjaroselo01@gmail.com>
2024-06-12 11:07:14 -0300

    Corrección de la actividad 2

--- src/main/java/actividad2/HistoriaUsuario.java
+++ src/main/java/actividad2/HistoriaUsuario.java
@@ -3,13 +3,13 @@ package actividad2;
 import java.util.List;
 
 public class HistoriaUsuario implements Item{
-    private List<Tarea> tareas;
-    public HistoriaUsuario(List<Tarea> tareas) {
-        this.tareas = tareas;
+    private List<Item> items;
+    public HistoriaUsuario(List<Item> items) {
+        this.items = items;
     }
     @Override
     public int calcularTiempo() {
-        return this.tareas.stream()
-                .mapToInt(t -> t.calcularTiempo()).sum();
+        return this.items.stream()
+                .mapToInt(i -> i.calcularTiempo()).sum();
     }
 }
